b924a2734997ed39f4eb2a7114d5d0b009231ef3
marius <mmillea@ucdavis.edu>
2017-06-05 14:10:08 +0200

    add Fisher plotting tools

--- Fisher.jl
+++ Fisher.jl
@@ -38,9 +38,10 @@ automatically aligned.
 """
 module Fisher
 
-export FisherMatrix, stds, corr, with_fixed
+export FisherMatrix, stds, corr, with_fixed, like1d, like2d, likegrid
 
-using NamedArrays
+using NamedArrays, PyCall, PyPlot
+@pyimport matplotlib.patches as patches
 import Base: +, show
 
 type FisherMatrix{Tvals,Tnames}
@@ -119,6 +120,62 @@ Get a Fisher matrix with some of the parameters held fixed.
 function with_fixed(params,f::FisherMatrix)
     ii = deleteat!(collect(1:length(f.names)),sort(indexin(params,f.names)))
     FisherMatrix(f.fish[ii,ii],f.names[ii])
+end
+
+prior(param,std::Real) = FisherMatrix(eye(1)/std^2,[param])
+
+
+# Plotting
+# --------
+
+function like1d{Tnames,Tvals}(fish::FisherMatrix{Tvals,Tnames}, param::Tnames, center=0; kwargs...)
+    like1d(center, stds(fish)[param]; kwargs...)
+end
+
+function like2d{Tnames,Tvals}(fish::FisherMatrix{Tvals,Tnames}, param1::Tnames, param2::Tnames, center=(0,0); kwargs...)
+    ii = indexin([param1,param2],fish.names)
+    @assert !any(ii.==0) "Fisher doesn't contain both parameters $param1 and $param2"
+    like2d(center, inv(fish.fish)[ii,ii]; kwargs...)
+end
+
+function like1d(μ,σ; σlim=4, color=nothing, kwargs...)
+    x = linspace(μ-4σ, μ+4σ,100)
+    plot(x,exp(-(x-μ).^2/2/σ^2); color=color, kwargs...)
+end
+
+function like2d(μ,Σ; σs=1:2, σlim=1.618max(σs...), color=nothing, kwargs...)
+    λ, v = eig(Σ)
+    θ = rad2deg(atan2(v[2,1],v[1,1]))
+    getc(σ) = imag(√2*√(complex(log(erfc(σ/√2)))))
+    if (color == nothing) color = pybuiltin("next")(gca()[:_get_lines][:prop_cycler])["color"] end
+    lw = get(plt[:rcParams],"lines.linewidth",1)
+    for c in map(getc,σs)
+        gca()[:add_artist](patches.Ellipse(;
+            xy=μ, width=2c*√λ[1], height=2c*√λ[2], angle=θ, lw=lw, 
+            facecolor="none", edgecolor=color, fill=false, kwargs...
+        ))
+    end
+    if σlim != nothing
+        xlim((μ[1]+x*getc(σlim)*√λ[1] for x=[-1,1])...)
+        ylim((μ[2]+x*getc(σlim)*√λ[2] for x=[-1,1])...)
+    end
+end
+
+function likegrid{Tnames,Tvals}(fish::FisherMatrix{Tvals,Tnames}, params::AbstractArray{Tnames}=fish.names; centers=Dict(), kwargs...)  
+    fig=gcf()
+    n=length(params)
+    for (i,p1) in enumerate(params)
+        for (j,p2) in enumerate(params)
+            if (i<=j)
+                ax=fig[:add_subplot](n,n,(j-1)*n+i)
+                if i==j 
+                    like1d(fish,p1,get(centers,p1,0); kwargs...)
+                else
+                    like2d(fish,p1,p2,(get(centers,p1,0),get(centers,p2,0)); kwargs...)
+                end
+            end
+        end
+    end
 end
 
 
